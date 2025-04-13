ae157254f4b4558ebb6a2ca5573b19819d028d52
Tal Jacob <taljacob2@gmail.com>
2025-04-13 20:12:35 +0300

    Extend Y Scroll
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- nextstep-frontend/src/components/Layout.tsx
+++ nextstep-frontend/src/components/Layout.tsx
@@ -5,7 +5,13 @@ import './Layout.css';
 
 const Layout: React.FC<{ children: React.ReactNode }> = (props: any) => {
   return (
-    <Container className="layout-container" component="main">
+    <Container className="layout-container" component="main"
+      sx={{
+        "&.MuiContainer-root": {
+          maxWidth: 'none'
+        }
+      }}
+    >
         { props.children }
     </Container>
   );
