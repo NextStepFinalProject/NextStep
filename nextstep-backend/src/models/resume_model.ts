fa4e9d756cef9b8da52ab29e7cdf47fa64190e5c
LiavTB <49226046+LiavTB@users.noreply.github.com>
2025-05-23 15:58:49 +0300

    Changed some structure created the resume model and types/resume_types.ts TODO - connect to a resume upload and parsing in the server, integrate with the client

---
+++ nextstep-backend/src/models/resume_model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from 'mongoose';
+import {ResumeData} from "types/resume_types";
+
+const ResumeSchema = new Schema({
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    version: { type: Number, required: true },
+    rawContentLink: { type: String, required: true },
+    parsedData: {
+        type: {
+            aboutMe: { type: String, required: false },
+            skills: { type: [String], required: false },
+            roleMatch: { type: String, required: false },
+            experience: { type: [String], required: false }
+
+        },
+        required: false
+    },
+    createdAt: { type: Date, default: Date.now }
+}, { versionKey: false });
+
+
+ResumeSchema.set('toJSON', {
+    transform: (doc, ret): ResumeData => {
+        return {
+            id: ret._id,
+            owner: ret.owner._id.toString(),
+            createdAt: ret.createdAt,
+            updatedAt: ret.updatedAt,
+            version: ret.version,
+            rawContentLink: ret.rawContentLink,
+            parsedData: ret.parsedData
+        };
+    }
+});
+
+export default mongoose.model('Resume', ResumeSchema);
