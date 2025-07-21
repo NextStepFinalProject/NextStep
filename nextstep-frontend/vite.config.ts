84e9a3c0f1e51269ff762206b1573713b379734b
Tal Jacob <taljacob2@gmail.com>
2025-07-21 09:41:42 +0300

    Allow Frontend To Be Hosted On Other Domain Names
    The Vite configuration has been updated to use
    the `VITE_DOMAIN_NAME` environment variable for
    the host in both the server and preview sections.
    Now, if the `.env` file sets
    `VITE_DOMAIN_NAME=0.0.0.0`, the frontend will be
    exposed on all network interfaces.
    
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- nextstep-frontend/vite.config.ts
+++ nextstep-frontend/vite.config.ts
@@ -9,7 +9,11 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.VITE_PORT || '5000')
+    port: parseInt(process.env.VITE_PORT || '5000'),
+    host: process.env.VITE_DOMAIN_NAME || 'localhost'
   },
-  preview: { port: parseInt(process.env.VITE_PORT || '5000') }
+  preview: {
+    port: parseInt(process.env.VITE_PORT || '5000'),
+    host: process.env.VITE_DOMAIN_NAME || 'localhost'
+  }
 })
